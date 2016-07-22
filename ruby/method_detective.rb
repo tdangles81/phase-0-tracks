f40fca8d133ea1928b95b46fe7aba68a91efea17
Ty Daniels <ty.multisonus@gmail.com>
2016-07-21 17:55:07 -0700

    Added method- detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+ "zom".insert(-2, 'o')
 # => “zoom”
 
-# "enhance".<???>
+ "enhance".prepend("   ")
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+ "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+ "the usual".append("suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+ " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+ "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+ "The mystery of the missing first letter".sub('T', '')
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+ 'z'.ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# The significance of the integer value is the ascii value of the character.
 
-# "How many times does the letter 'a' appear in this string?".<???>
+ "How many times does the letter 'a' appear in this string?".count 'a'
 # => 4
