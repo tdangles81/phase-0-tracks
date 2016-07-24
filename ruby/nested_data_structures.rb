dc40fea1597797dff35c7d60c5be60956739c754
Ty Daniels <ty.multisonus@gmail.com>
2016-07-23 18:04:09 -0700

    Added nested data structures.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,42 @@
+cities = {
+
+	landmarks: {
+	san_francisco: {
+		sf_landmarks: [
+			'Golden Gate Bridge', 'AT&T Park', 
+			sf_slogan: 'The best city ever!'
+		]
+		},
+	new_york: {
+		ny_landmarks: [
+			'Empire State', 'Statue of Liberty',
+			ny_slogan: 'The best city ever!'
+		]
+		},
+	austin: {
+		austin_landmarks: [
+			'SRV Grave Site', 'State Capital',
+			austin_slogan: 'The best city ever!'
+		]
+	},
+	berkeley: {
+		berk_landmarks: [
+			'UC Berkeley', 'Indian Rock',
+			berk_slogan: 'The best city ever!'
+		]
+	},
+	san_diego: {
+		sd_landmarks: [
+			'US Border', 'Ocean Beach',
+			sd_slogan: 'The best city ever!'
+		]
+	}
+	}
+
+}
+
+cities[:landmarks][:san_francisco][:sf_landmarks][2][:sf_slogan] = 'home of the giants'.upcase
+
+p cities[:landmarks][:san_diego][:sd_landmarks]
+
+p cities[:landmarks][:san_francisco][:sf_landmarks][2][:sf_slogan]
