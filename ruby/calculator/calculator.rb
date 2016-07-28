9f3c9240d272ea2756edcdcb0a369918237f0337
Ty Daniels <ty.multisonus@gmail.com>
2016-07-28 01:05:11 -0700

    Added santa.rb reindeer attributes

--- ruby/calculator/calculator.rb
+++ ruby/calculator/calculator.rb
@@ -1,41 +1,45 @@
 puts "Add an equation with the format: '1 + 5'. Program performs sum, difference, and product calculations."
 init_input = gets.chomp
+$calculations_store = []
 
-def calculator(input)
-	equation_store = []
-	calculations_store = []
+def calculator(num1, operator, num2)
 
-	while input != 'done'
-
-	equation_store << input
-	p equation_store
-	formatted_input = input.split(' ')
-
-	if formatted_input[1] == '+'
-		new_sum = formatted_input[0].to_i + formatted_input[2].to_i
+	if operator == '+'
+		new_sum = num1.to_i + num2.to_i
 		puts new_sum
-	elsif formatted_input[1] == '-'
-		new_sum = formatted_input[0].to_i - formatted_input[2].to_i
+	elsif operator == '-'
+		new_sum = num1.to_i - num2.to_i
 		puts new_sum
-	elsif formatted_input[1] == '*'
-		new_sum = formatted_input[0].to_i * formatted_input[2].to_i
+	elsif operator == '*'
+		new_sum = num1.to_i * num2.to_i
 		puts new_sum
 	else 
 		puts "Your math could use some work, or program doesn't handle this kind of equation. Try again."
 	end
+	$calculations_store << new_sum
+end
 
-	calculations_store << new_sum
-	p calculations_store
-	puts "Add a new equation with the format: '1 + 5'. Input 'done' to check eq'ns."
-	input = gets.chomp
+def convert_to_equation(input)
+	equation_store = []
+
+	while input != 'done'
+		equation_store << input
+		p equation_store
+		string_to_convert = input.split(' ')
+		p string_to_convert
+		calculator(string_to_convert[0], string_to_convert[1], string_to_convert[2])
+		"Add a new equation with the format: '1 + 5'. Input 'done' to check eq'ns."
+		input = gets.chomp
 	end
 
 	puts "Calculations performed:"
 	count = 0
 	while count < equation_store.length
-		puts "#{equation_store[count]} = #{calculations_store[count]}"
+		puts "#{equation_store[count]} = #{$calculations_store[count]}"
 		count += 1
 	end
 end
 
-p calculator(init_input)
+convert_to_equation(init_input)
+
+
