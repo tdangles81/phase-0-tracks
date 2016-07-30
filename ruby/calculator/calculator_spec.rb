b596b05eb1dcd9ccb2c5774ddfeaf427f6640687
Ty Daniels <ty.multisonus@gmail.com>
2016-07-30 09:33:12 -0700

    Added gps2_2.rb

---
+++ ruby/calculator/calculator_spec.rb
@@ -0,0 +1,31 @@
+# our calculator should ...
+# add two integers and return an integer
+# subtract two integers and return an integer
+# multiply two integers and return an integer
+
+# RSpec
+# 'describe' block for each group of tests
+  # 'it' block for each individual test
+    # expect(<YOUR CODE>).to eq <RESULT>
+
+require_relative 'calculator_new'
+
+describe Calculator do
+  let(:calculator) { Calculator.new }
+
+  it "adds two integers" do
+    expect(calculator.add(3,4)).to eq 7
+  end
+
+  it "subtracts two integers" do
+    expect(calculator.subtract(7,1)).to eq 6
+  end
+
+  it "multiplies two integers" do
+    expect(calculator.multiply(2,3)).to eq 6
+  end
+
+  it "divides two integers" do
+    expect(calculator.divide(10,2)).to eq 5
+  end
+end
