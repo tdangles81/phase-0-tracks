f443cb9db5a944c355916dc8763b28b1682f296b
Ty Daniels <ty.multisonus@gmail.com>
2016-08-04 02:14:23 -0700

    Refactored with comments

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,7 @@
+//Create the ability to input a word or sentence, and begin reading the word from
+//the last character. Reading the input in reverse, take each letter and place into a new string, 
+//forming a new word. This will cause initial word to appear in reverse when printed.
+
 function Reverse(string){
 	char = '';
 	for (var i = string.length; i >= 0; i--)
