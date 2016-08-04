a6d95d2ecd4fb80536c236c725257882b22142cf
Ty Daniels <ty.multisonus@gmail.com>
2016-08-04 02:04:49 -0700

    Added explore.js with working reverse string function.

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+function Reverse(string){
+	char = '';
+	for (var i = string.length; i >= 0; i--)
+		char += string[i];
+		return char;
+	}
+
+Reverse("This is my super short string")
+
+if (char.length < 100) {
+	console.log(char);
+}else {
+	console.log("Woops, that string is too long!");
+}
