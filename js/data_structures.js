29470b3024409c859685614fa2bef44e4d61879b
Ty Daniels <ty.multisonus@gmail.com>
2016-08-06 13:20:30 -0700

    Added comments

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,9 +1,12 @@
+//Takes two groups of information and combines their individual pieces of information.
+//For example, information stored in the first group such as a name might be used to 
+//reference data in the second.
+
 var colorarray = ["blue", "green", "yellow", "red"]
 var namesarray = ["Ringo", "John", "Paul", "George"]
 var horse = []
 
 if (colorarray.length == namesarray.length) {
-
 	for (var i = 0; i < namesarray.length; i++) {
 		horse[namesarray[i]] = colorarray[i]
 	}
